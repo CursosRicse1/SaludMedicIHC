c84beb619c26a10308ba90d332fef125556b8a5a
Anthony-Ricse <dokouser98@gmail.com>
2021-11-23 14:00:16 -0300

    LoadingAndError

--- saludmedic/backend/server.js
+++ saludmedic/backend/server.js
@@ -4,9 +4,10 @@ const app = express();
 app.get('/' , (req, res) => {
     res.send('El servidor esta operando')
 });
+const port = process.env.PORT || 5000; 
 
-app.listen(5000, () =>{
-    console.log('Servidor en http://localhost:5000');
+app.listen(port, () =>{
+    console.log( `Servidor en http://localhost:${port}`  );
 })
 app.get('/api/users',(req,res)=>{
     res.send(data.usuarios);
--- saludmedic/frontend/src/screens/HomeScreen.js
+++ saludmedic/frontend/src/screens/HomeScreen.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+    const [loading , setLoading] = useState(false);
+    const[error , setError] = useState(false);
+
+    useEffect(()=>{
+        try{
+            setLoading(true);
+            setLoading(false);
+        }catch(err){
+            setError(err.message);
+            setLoading(false);
+        }
+    }, [] )
     return (
+        <div> 
+        {loading ? ( <LoadingBox></LoadingBox>
+        ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+
+
+
         <div>
-          
-                HomeScreen
-            
+            HomeScreen
+            <div className="bg-blue-600 px-5">
+                ga
+            </div>
         </div>
+        
+     
+        )}
+    </div>
     )
 }
