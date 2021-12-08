4543759edf598a472f21865f56afeca7d5da9617
Anthony-Ricse <dokouser98@gmail.com>
2021-12-08 11:27:50 -0300

    avance

--- saludmedic/backend/server.js
+++ saludmedic/backend/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const app = express()
+const app = express();
 
 const db = require('./models');
 
-db.sequelize
+//Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(5000 , () =>{
+    console.log("Servidor corriendo en el puerto 5000");
+  });
+});
 
 
-
-
-app.get('/' , (req , res )=>{
-  res.send("servidor operando ");
-})
-app.listen(5000 , ()=>{
-  console.log("server operando en el puerto 5000");
+app.get('/' , (req , res ) =>{
+  res.send("Serivdor operando")
 })
