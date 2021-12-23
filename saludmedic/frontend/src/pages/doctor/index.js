61bb9d76b7a42cafecddafbce8800a883838c8de
Anthony-Ricse <dokouser98@gmail.com>
2021-12-23 17:24:19 -0300

    defuncion

--- saludmedic/frontend/src/pages/doctor/index.js
+++ saludmedic/frontend/src/pages/doctor/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function DoctorScreen() {
+<<<<<<< HEAD
+  return (
+    <div>
+      <h1 className="bg-green-600 h-10 w-36">DoctorScreen</h1>
+    </div>
+  );
+=======
     return (
         <div>
           
@@ -11,4 +18,5 @@ export default function DoctorScreen() {
           
         </div>
     )
+>>>>>>> defuncion
 }
